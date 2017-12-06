f74bef6fecfbcf5be4214ff4b73047f0d87b5297
annapismenna <pismennaanna@gmail.com>
2017-12-06 20:50:00 +0700

    functional and class component, input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-//create new component, this component should
-//produce some html
-const App = function () {
-    return <div>Hi</div>;
+import SearchBar from './components/search_bar'; //компонент поиска
+
+const API_KEY = 'AIzaSyCOrCVYNL_Pv8TOrZN1rRr1wkDr9rxiEms';
+
+//create new component, this component should produce some html
+
+const App = () => { //functional base component
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //take this component's generated html and put it on the page (in the DOM)
-React.render(App);
+
+ReactDOM.render(<App />, document.querySelector('.container'));
